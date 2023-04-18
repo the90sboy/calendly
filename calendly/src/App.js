43868448adad123d65e55fd1be7ac6a26835cf5e
KAFUI AYEKPLE <ayekple360@gmail.com>
2023-04-18 09:39:51 +0000

    my calendly react

---
+++ calendly/src/App.js
@@ -0,0 +1,13 @@
+import images from './images';
+import './App.css';
+import navbar from './navbar';
+import react from "react"
+function App() {
+  return (
+    <div>
+      <navbar />
+    </div>
+  )
+}
+
+export default App;
